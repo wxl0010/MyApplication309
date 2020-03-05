335b2382891e6c990fd987214285787326fe47f3
wxl <18807820020@139.com>
2020-03-05 20:54:27 +0800

    C4

---
+++ app/src/main/java/cn/edu/sziit/chapter1_logcat/MainActivity.java
@@ -0,0 +1,56 @@
+package cn.edu.sziit.chapter1_logcat;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*initData();*/
+
+    }
+
+    private void initData() {
+        String TAG=getLocalClassName().toString();
+        Log.d(TAG, "initData:调试信息 ");
+        Log.v(TAG,"initData:冗余信息");
+        Log.i(TAG, "initData: 普通信息");
+        Log.w(TAG, "initData: 警告信息");
+        Log.e(TAG, "initData: 错误信息");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+}
